397317018411c13ca7335a1cfd6c62443c319a8a
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2025-01-12 18:38:47 +0530

    Update TopRestaurant.jsx

--- Swiggy Clone/src/components/TopRestaurant.jsx
+++ Swiggy Clone/src/components/TopRestaurant.jsx
@@ -1,40 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
+import RestaurantCards from "./RestaurantCards";
 
-function TopRestaurant() {
-    const [value,setValue]= useState(0);
+function TopRestaurant({ data }) {
+  const [value, setValue] = useState(0);
 
-
-    function handlePrev(){
-        setValue((prev)=>prev-20)
-        
-    }
-    function handleNext(){
-        setValue((prev)=>prev+20)
-
-    }
+  function handlePrev() {
+    value <= 0 ? "" : setValue((prev) => prev - 269);
+  }
+  function handleNext() {
+    value>=1869?"":setValue((prev) => prev + 269);
+    console.log(value);
+  }
   return (
     <div>
-        
-    <div className='flex justify-between mt-16'>
-        <h1 className='font-bold text-3xl'>
-            Top Restaurant chains in Delhi 
-        </h1>
-        <div className='flex gap-3 '>
-        <div onClick={handlePrev} className={`bg-gray-200 cursor-pointer  rounded-full w-9 h-9 flex justify-center items-center ` + (value <=0?"bg-gray-50":"bg-gray-200")}>
-            <i className={`fi text-2xl mt-1 fi-rr-arrow-small-left ` + (value <=0?"text-gray-300":"text-gray-800") }></i>
-            </div>
-            <div onClick={handleNext} className={`bg-gray-200 cursor-pointer rounded-full w-9 h-9 flex justify-center items-center ` + (value >=180?"bg-gray-50":"bg-gray-200")}>
-            <i className={`fi text-2xl mt-1 fi-rr-arrow-small-right ` + (value >=180?"text-gray-300":"text-gray-800") }></i>
-            </div>
-
+      <div className="flex justify-between mt-12">
+        <h1 className="font-bold text-2xl">Top Restaurant chains in Delhi</h1>
+        <div className="flex gap-3 ">
+          <div
+            onClick={handlePrev}
+            className={
+              `bg-gray-200 cursor-pointer  rounded-full w-9 h-9 flex justify-center items-center ` +
+              (value <= 0 ? "bg-gray-50" : "bg-gray-200")
+            }
+          >
+            <i
+              className={
+                `fi text-2xl mt-1 fi-rr-arrow-small-left ` +
+                (value <= 0 ? "text-gray-300" : "text-gray-800")
+              }
+            ></i>
+          </div>
+          <div
+            onClick={handleNext}
+            className={
+              `bg-gray-200 cursor-pointer rounded-full w-9 h-9 flex justify-center items-center ` +
+              (value >= 1869 ? "bg-gray-50" : "bg-gray-200")
+            }
+          >
+            <i
+              className={
+                `fi text-2xl mt-1 fi-rr-arrow-small-right ` +
+                (value >= 1869 ? "text-gray-300" : "text-gray-800")
+              }
+            ></i>
+          </div>
         </div>
+      </div>
+      <div className={`flex mt-4 gap-10 w-full `}>
+        {data.map(({info}) => (
+          <div
+            className=" hover:scale-95 duration-300"
+            style={{ translate: `-${value}%` }}
+          >
+            <RestaurantCards {...info}/>
+          </div>
+        ))}
+      </div>
+      {/* <hr className="border" /> */}
     </div>
-    <div  className={`flex mt-4 `}>
-
-    </div>
-    <hr className='border' />
-</div>
-  )
+  );
 }
 
-export default TopRestaurant
+export default TopRestaurant;
