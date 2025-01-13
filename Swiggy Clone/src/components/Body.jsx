01caa83989584ac3a27f6e512a298b50aa17cf29
Abhishek Kumar Mohanty <abhishekmohanty7325@gmail.com>
2025-01-13 22:18:34 +0530

    Update Body.jsx

--- Swiggy Clone/src/components/Body.jsx
+++ Swiggy Clone/src/components/Body.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import Cusine from './Cusine'
 import TopRestaurant from './TopRestaurant'
+import Head from './Head'
 
 function Body() {
+  const [topRestaurantData,setTopRestaurantData] = useState([])
+  const [cusine,setCusine] = useState([])
+  async function fetchData() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type="
+    );
+    const result = await data.json();
+    setTopRestaurantData(result?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setCusine(result?.data?.cards[0]?.card?.card?.imageGridCards?.info);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className='w-full '>
+        <div className='w-[73.5%] mx-auto  overflow-hidden '>
 
-        <div className='w-[75%] mx-auto  mt-4   overflow-hidden '>
-
-            <Cusine/>
-            <TopRestaurant/>
+            <Cusine data ={cusine}/>
+            <TopRestaurant data={topRestaurantData}/>
         </div>
     </div>
   )
